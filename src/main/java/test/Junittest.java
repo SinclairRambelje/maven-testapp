e7d8365ae69400b5a188c0d98f60d3fc737cf76f
Sinclair Rambelje <sinne_pin@hotmail.com>
2018-02-28 13:23:08 +0100

    add junit test
    junit test

---
+++ src/main/java/test/Junittest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Junittest {
+
+    @Test
+    public void mocktest() {
+
+        assertEquals(1, 1);
+    }
+}
